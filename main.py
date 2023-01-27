393f4d6c96dd9b95cc4d045c70838e84ce778a06
Sohlar <drew@robosim.com>
2023-01-27 13:24:47 -0800

    Added Sub-Plotting for Numerical and Categorical Predictions

--- main.py
+++ main.py
@@ -1,40 +1,69 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.metrics import mean_absolute_error, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+import matplotlib.pyplot as plt
 
 train_data = pd.read_csv('data/train.csv')
 
 test_data = pd.read_csv('data/test.csv')
 
 
+#Split training data into numerical and categorical dataframes
+categorical_train_data = train_data.select_dtypes(include=['object'])
+numerical_train_data = train_data.select_dtypes(include=['int64'])
+Sale_price_data = train_data.SalePrice
 
-
-
-encoder = OneHotEncoder()
 print(train_data.shape)
-train_data = pd.get_dummies(train_data, columns=train_data.select_dtypes(include='object').columns)
-train_data = train_data.dropna(axis=0)
-print(train_data.shape)
-'''
-for feature in feature_list:
-    print(train_data[feature].dtype)
-    if train_data[feature].dtype == 'int64':
-        train_data[feature] = encoder.fit_transform(train_data[[feature]])
-'''
 
-#feature_list_encoded = [encoder.fit_transform(train_data[[feature]]) for feature in feature_list if train_data[feature].dtype == object]
-feature_list = [feature for feature in train_data.columns if feature != 'SalePrice' ]
-x = train_data[feature_list]
+#Converts cateogircal dataframe into indicator variables
+encoded_categorical_train_data = pd.get_dummies(categorical_train_data)
+print(train_data.shape)
+#Drops NaN values
+encoded_categorical_train_data = encoded_categorical_train_data.dropna(axis=0)
+numerical_train_data = numerical_train_data.dropna(axis=0)
+print(encoded_categorical_train_data.shape)
+print(numerical_train_data.shape)
+
+
+numerical_feature_list = [feature for feature in numerical_train_data.columns if feature != 'SalePrice' ]
+categorical_feature_list = [feature for feature in encoded_categorical_train_data.columns]
+
+#This might be wrong, Since we split the dataframe I may not need to signify columns because they only exist within the dataframe
+#Except with numerical data we do not want saleprice
+#We could potentially improve speed by dropping saleprice from df therefore no longer needing to check when building feature list
+numerical_x = numerical_train_data[numerical_feature_list]
+categorical_x = encoded_categorical_train_data[categorical_feature_list]
 
 y = train_data.SalePrice
 
+#Numerical Regression
+train_numerical_x, val_numerical_x, train_numerical_y, val_numerical_y = train_test_split(numerical_x, y, test_size=0.2, random_state=0)
+forest_regression_model = RandomForestRegressor(random_state = 1)
+forest_regression_model.fit(train_numerical_x, train_numerical_y)
+regression_predictions = forest_regression_model.predict(val_numerical_x)
+print(len(val_numerical_y))
+print(len(regression_predictions))
 
-train_x, val_x, train_y, val_y = train_test_split(x, y, test_size=0.2, random_state=0)
+print('Regression MAE: ' + str(mean_absolute_error(val_numerical_y, regression_predictions)))
+plt.subplot(1, 2, 1)
+plt.plot(regression_predictions, color ='r')
+plt.plot(val_numerical_y.values, color ='b')
 
-forest_model = RandomForestRegressor(random_state = 1)
-forest_model.fit(train_x, train_y)
-predictions = forest_model.predict(val_x)
+#categorical Classification
+train_categorical_x, val_categorical_x, train_categorical_y, val_categorical_y = train_test_split(categorical_x, y, test_size=0.2, random_state=0)
+forest_classification_model = RandomForestClassifier(n_estimators=100, random_state=0)
+forest_classification_model.fit(train_categorical_x, train_categorical_y)
+classification_predictions = forest_classification_model.predict(val_categorical_x)
+print(len(val_categorical_y))
+print(len(classification_predictions))
 
-print(mean_absolute_error(val_y, predictions))
+print('Classification MAE: ' + str(mean_absolute_error(val_categorical_y, classification_predictions)))
+plt.subplot(1, 2, 2)
+plt.plot(classification_predictions, color ='r')
+plt.plot(val_categorical_y.values, color ='b')
+plt.show()
+
+ovr_pred = (classification_predictions + regression_predictions) / 2
+
